0f4421aee099c39744d3601bd77fff090cc97b36
Alex Resnick <adr8292@gmail.com>
2025-01-15 14:28:51 -0600

    add dashboard template

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -3,13 +3,13 @@ import * as ReactDOM from 'react-dom/client';
 import { ThemeProvider } from '@mui/material/styles';
 import { CssBaseline } from '@mui/material';
 import theme from './theme';
-import App from './App';
+import App from './Dashboard';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>,
+    <React.StrictMode>
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <App />
+        </ThemeProvider>
+    </React.StrictMode>,
 );
