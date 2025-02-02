ae197fa0c949ce08ffe1236ac4c0d6f4ae945d31
Alex Resnick <adr8292@gmail.com>
2025-02-02 21:11:20 +0000

    fix build

--- web/src/components/UserOrg.tsx
+++ web/src/components/UserOrg.tsx
@@ -14,43 +14,43 @@ function test() {
         return <Typography>No user session available</Typography>;
     }
 
-    const { logo: orgLogo, name: orgName, url: orgUrl } = session.session?.org;
-    return (
-        <Stack>
-            <AccountPreview variant="expanded" />
-            {session.session?.org && (
-                <Stack mb={1}>
-                    <Typography textAlign="center" fontSize="1rem" gutterBottom>
-                        {session?.session?.admin && <div>Admin</div>}
-                    </Typography>
-                    {/* <Box display="flex" justifyContent="center" alignItems="center" gap={2}>
-                        <Avatar
-                            variant="square"
-                            src={orgLogo}
-                            alt={orgName}
-                            sx={{ width: 27, height: 24 }}
-                        />
-                        <Stack>
-                            <Typography variant="caption" fontWeight="bolder">
-                                {orgName}
-                            </Typography>
-                            <Link
-                                variant="caption"
-                                href={orgUrl}
-                                target="_blank"
-                                rel="noopener noreferrer"
-                            >
-                                {orgUrl}
-                            </Link>
-                        </Stack>
-                    </Box> */}
-                </Stack>
-            )}
-            <Divider />
-            <AccountPopoverFooter>
-                <SignOutButton />
-            </AccountPopoverFooter>
-        </Stack>)
+    // const { logo: orgLogo, name: orgName, url: orgUrl } = session.session?.org;
+    // return (
+    //     <Stack>
+    //         <AccountPreview variant="expanded" />
+    //         {session.session?.org && (
+    //             <Stack mb={1}>
+    //                 <Typography textAlign="center" fontSize="1rem" gutterBottom>
+    //                     {session?.session?.admin && <div>Admin</div>}
+    //                 </Typography>
+    //                 {/* <Box display="flex" justifyContent="center" alignItems="center" gap={2}>
+    //                     <Avatar
+    //                         variant="square"
+    //                         src={orgLogo}
+    //                         alt={orgName}
+    //                         sx={{ width: 27, height: 24 }}
+    //                     />
+    //                     <Stack>
+    //                         <Typography variant="caption" fontWeight="bolder">
+    //                             {orgName}
+    //                         </Typography>
+    //                         <Link
+    //                             variant="caption"
+    //                             href={orgUrl}
+    //                             target="_blank"
+    //                             rel="noopener noreferrer"
+    //                         >
+    //                             {orgUrl}
+    //                         </Link>
+    //                     </Stack>
+    //                 </Box> */}
+    //             </Stack>
+    //         )}
+    //         <Divider />
+    //         <AccountPopoverFooter>
+    //             <SignOutButton />
+    //         </AccountPopoverFooter>
+    //     </Stack>)
 }
 export function UserOrg() {
     return (
